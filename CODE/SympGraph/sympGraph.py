5a9c3d733b55e39e92956783b4fa1f3088408e4e
Devi1X <56777058+devi1X@users.noreply.github.com>
2023-04-08 20:22:23 -0700

    sympgraph

--- CODE/SympGraph/sympGraph.py
+++ CODE/SympGraph/sympGraph.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 import networkx as nx
 import pandas as pd
 def getPostWiseSymptoms(numPosts):
-    file = 'merged_file.csv'
+    file = 'merged_result.csv'
     allpostSymptoms = {}
     allSymptoms = {}
     symtomsReverseMap = {}
